43fcfffec391a1fbb3829b49d2ef45f856fac7cd
Daniel Cirincione <danny.cirincione@gmail.com>
2026-06-15 21:52:51 -0400

    VISUAL- added banner to merch page with message and link to community

--- app/merch/page.tsx
+++ app/merch/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
 
 import "./merch.css";
 
@@ -22,6 +23,17 @@ export default async function MerchPage() {
   return (
     <PageShell>
       <Section id="merch-page" className="merch-section" title="Merchandise" headingLevel="h1" showHeader={false}>
+        <div className="merch-fund-banner" aria-label="Aldrich Sports Fund announcement">
+          <div className="merch-fund-banner__inner">
+            <span className="merch-fund-banner__label">Aldrich Sports Fund</span>
+            <p className="merch-fund-banner__message">
+              For every piece of clothing sold, we put $1 to the Aldrich Sports Fund
+            </p>
+            <Link className="button ghost merch-fund-banner__button" href="/community">
+              Learn More
+            </Link>
+          </div>
+        </div>
         <MerchStorefront catalog={catalog} purchasesEnabled={siteSettings.merch.purchasesEnabled} />
       </Section>
     </PageShell>
